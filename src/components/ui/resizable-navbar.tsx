78d57db4338578388ca4f9a3de07f0af7338caf4
ismoiljondev <ismoiljonhalilov5@gmail.com>
2025-09-17 15:44:53 +0500

    fix

--- src/components/ui/resizable-navbar.tsx
+++ src/components/ui/resizable-navbar.tsx
@@ -46,7 +46,7 @@ interface MobileNavMenuProps {
     children: React.ReactNode;
     className?: string;
     isOpen: boolean;
-    // onClose: () => void;
+    onClose: () => void;
 }
 
 export const Navbar = ({ children, className }: NavbarProps) => {
