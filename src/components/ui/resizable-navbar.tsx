9abadc01dd9da5df6b23a51919aa21fd83eb296f
ismoiljondev <ismoiljonhalilov5@gmail.com>
2025-10-17 10:37:56 +0500

    feat navbar

--- src/components/ui/resizable-navbar.tsx
+++ src/components/ui/resizable-navbar.tsx
@@ -7,6 +7,9 @@ import {
     useScroll,
     useMotionValueEvent,
 } from "motion/react";
+import { useLocale, useTranslations } from "next-intl";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 import React, { useRef, useState } from "react";
 
@@ -69,7 +72,7 @@ export const Navbar = ({ children, className }: NavbarProps) => {
         <motion.div
             ref={ref}
             // IMPORTANT: Change this to class of `fixed` if you want the navbar to be fixed
-            className={cn("sticky inset-x-0 top-20 z-40 w-full", className)}
+            className={cn("sticky inset-x-0 top-1 z-40 w-full", className)}
         >
             {React.Children.map(children, (child) =>
                 React.isValidElement(child)
@@ -91,7 +94,7 @@ export const NavBody = ({ children, className, visible }: NavBodyProps) => {
                 boxShadow: visible
                     ? "0 0 24px rgba(34, 42, 53, 0.06), 0 1px 1px rgba(0, 0, 0, 0.05), 0 0 0 1px rgba(34, 42, 53, 0.04), 0 0 4px rgba(34, 42, 53, 0.08), 0 16px 68px rgba(47, 48, 55, 0.05), 0 1px 0 rgba(255, 255, 255, 0.1) inset"
                     : "none",
-                width: visible ? "40%" : "100%",
+                width: visible ? "95%" : "100%",
                 y: visible ? 20 : 0,
             }}
             transition={{
@@ -100,10 +103,10 @@ export const NavBody = ({ children, className, visible }: NavBodyProps) => {
                 damping: 50,
             }}
             style={{
-                minWidth: "800px",
+                minWidth: "1000px",
             }}
             className={cn(
-                "relative z-[60] mx-auto hidden w-full max-w-7xl flex-row items-center justify-between self-start rounded-full bg-transparent px-4 py-2 lg:flex dark:bg-transparent",
+                "relative z-[60] mx-auto hidden w-full max-w-7xl flex-row items-center justify-between self-start rounded-xl bg-transparent px-4 py-3 lg:flex dark:bg-transparent border",
                 visible && "bg-white/80 dark:bg-neutral-950/80",
                 className,
             )}
@@ -115,7 +118,9 @@ export const NavBody = ({ children, className, visible }: NavBodyProps) => {
 
 export const NavItems = ({ items, className, onItemClick }: NavItemsProps) => {
     const [hovered, setHovered] = useState<number | null>(null);
-
+    const t = useTranslations("menu");
+    const param = usePathname()
+    const locale = useLocale();
     return (
         <motion.div
             onMouseLeave={() => setHovered(null)}
@@ -125,21 +130,21 @@ export const NavItems = ({ items, className, onItemClick }: NavItemsProps) => {
             )}
         >
             {items.map((item, idx) => (
-                <a
+                <Link
                     onMouseEnter={() => setHovered(idx)}
                     onClick={onItemClick}
-                    className="relative px-4 py-2 text-neutral-600 dark:text-neutral-300"
+                    className={`relative px-4 py-2 text-neutral-600 dark:text-neutral-300 ${param === `/${locale}${item.link}` ? " bg-gray-100 dark:bg-neutral-800 rounded-lg border" : ""}`}
                     key={`link-${idx}`}
                     href={item.link}
                 >
                     {hovered === idx && (
                         <motion.div
                             layoutId="hovered"
-                            className="absolute inset-0 h-full w-full rounded-full bg-gray-100 dark:bg-neutral-800"
+                            className="absolute inset-0 h-full w-full rounded-lg bg-gray-100 dark:bg-neutral-800"
                         />
                     )}
-                    <span className="relative z-20">{item.name}</span>
-                </a>
+                    <span className="relative z-20 text-lg">{t(item.name)}</span>
+                </Link>
             ))}
         </motion.div>
     );
@@ -166,7 +171,7 @@ export const MobileNav = ({ children, className, visible }: MobileNavProps) => {
             }}
             className={cn(
                 "relative z-50 mx-auto flex w-full max-w-[calc(100vw-2rem)] flex-col items-center justify-between bg-transparent px-0 py-2 lg:hidden",
-                visible && "bg-white/80 dark:bg-neutral-950/80",
+                visible && "bg-white/80 dark:bg-neutral-950/80 !rounded-xl",
                 className,
             )}
         >
@@ -195,7 +200,7 @@ export const MobileNavMenu = ({
     children,
     className,
     isOpen,
-    onClose,
+    // onClose,
 }: MobileNavMenuProps) => {
     return (
         <AnimatePresence>
@@ -233,7 +238,7 @@ export const MobileNavToggle = ({
 export const NavbarLogo = () => {
     return (
         <a
-            href="#"
+            href="/"
             className="relative z-20 mr-4 flex items-center space-x-2 px-2 py-1 text-sm font-normal text-black"
         >
             <img
@@ -242,7 +247,7 @@ export const NavbarLogo = () => {
                 width={30}
                 height={30}
             />
-            <span className="font-medium text-black dark:text-white">Startup</span>
+            <span className="font-medium text-black dark:text-white text-xl">MilliySim</span>
         </a>
     );
 };
