427692b3616c4626d1a3217c4e6fa1a1eb09ac2b
ismoiljondev <ismoiljonhalilov5@gmail.com>
2025-10-17 10:15:11 +0500

    feat globus

---
+++ src/app/[locale]/esim/[id]/faq.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Faq = () => {
+    return (
+        <div className='grid grid-cols-2 gap-6'>
+
+        </div>
+    )
+}
+
+export default Faq
--- src/app/[locale]/esim/[id]/steps.tsx
+++ src/app/[locale]/esim/[id]/steps.tsx
@@ -10,21 +10,28 @@ const Steps = () => {
         <div className='flex flex-col gap-4'>
             <Title title={t("title")} desc='' className='text-start justify-start items-start' />
             <div className='grid grid-cols-3 gap-6'>
-                <div className='flex flex-col items-center text-center gap-2 border rounded-xl p-3 hover:cursor-pointer hover:scale-110 duration-800'>
-                    <IconNumber1 strokeWidth={1} size={70} />
-                    <p className='text-xl font-medium'>{t("step1_title")}</p>
-                    <p className='text-sm'>{t("step1_desc")}</p>
+                <div className='relative flex flex-col items-center text-center gap-2 border rounded-xl p-3 hover:cursor-pointer hover:scale-110 duration-800'>
+                    <div className='absolute top-3 left-3 border rounded-lg p-1'><IconNumber1 strokeWidth={1} size={30} /></div>
+                    <div className='flex flex-col mt-10'>
+                        <p className='text-xl font-medium'>{t("step1_title")}</p>
+                        <p className='text-sm'>{t("step1_desc")}</p>
+                    </div>
                 </div>
-                <div className='flex flex-col items-center text-center gap-2 border rounded-xl p-3 hover:cursor-pointer hover:scale-110 duration-800'>
-                    <IconNumber2 strokeWidth={1} size={70} />
-                    <p className='text-xl font-medium'>{t("step2_title")}</p>
-                    <p className='text-sm'>{t("step2_desc")}</p>
+                <div className='relative flex flex-col items-center text-center gap-2 border rounded-xl p-3 hover:cursor-pointer hover:scale-110 duration-800'>
+                    <div className='absolute top-3 left-3 border rounded-lg p-1'><IconNumber2 strokeWidth={1} size={30} /></div>
+                    <div className='flex flex-col mt-10'>
+                        <p className='text-xl font-medium'>{t("step2_title")}</p>
+                        <p className='text-sm'>{t("step2_desc")}</p>
+                    </div>
                 </div>
-                <div className='flex flex-col items-center text-center gap-2 border rounded-xl p-3 hover:cursor-pointer hover:scale-110 duration-800'>
-                    <IconNumber3 strokeWidth={1} size={70} />
-                    <p className='text-xl font-medium'>{t("step3_title")}</p>
-                    <p className='text-sm'>{t("step3_desc")}</p>
+                <div className='relative flex flex-col items-center text-center gap-2 border rounded-xl p-3 hover:cursor-pointer hover:scale-110 duration-800'>
+                    <div className='absolute top-3 left-3 border rounded-lg p-1'><IconNumber3 strokeWidth={1} size={30} /></div>
+                    <div className='flex flex-col mt-10'>
+                        <p className='text-xl font-medium'>{t("step3_title")}</p>
+                        <p className='text-sm'>{t("step3_desc")}</p>
+                    </div>
                 </div>
+
 
             </div>
         </div>
